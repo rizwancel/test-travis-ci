3d0d9315a83ff3b54eac9ebdf5cd5335ca0b0c44
Rizwan Ansari <rizwan@celestialsys.com>
2020-10-22 14:23:06 +0530

    Python 3.8.0

--- build_python_at14.sh
+++ build_python_at14.sh
@@ -5,20 +5,17 @@ working_dir=${PWD}
 
 apt-get install -y libssl-dev libreadline-dev libreadline-dev libbz2-dev libgdbm-dev libsqlite3-dev zlib1g-dev liblzma-dev libtk-img-dev libffi-dev liblzma-dev
 
-wget https://www.python.org/ftp/python/3.6.6/Python-3.6.6.tgz
+wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
 
-tar -xf Python-3.6.6.tgz
+tar -xf Python-3.8.0.tgz
 
-cd Python-3.6.6
+cd Python-3.8.0
 
-PYTHON_LOCAL=/opt/python366-at14
+PYTHON_LOCAL=/opt/python380-at14
 
 echo "Python local = $PYTHON_LOCAL"
 
 echo "PWD = ${PWD}"
-
-#./configure  --with-lto --enable-optimizations --enable-ipv6 --enable-loadable-sqlite-extensions --prefix=${PYTHON_LOCAL} --exec-prefix=${PYTHON_LOCAL}  > configure_logs 2>&1
-#./configure  --with-lto --enable-ipv6 --enable-loadable-sqlite-extensions --prefix=${PYTHON_LOCAL} --exec-prefix=${PYTHON_LOCAL}
 
 ./configure CC="/opt/at14.0/bin/gcc" CXX="/opt/at14.0/bin/g++" --with-lto --enable-optimizations --enable-ipv6 --enable-loadable-sqlite-extensions --prefix=${PYTHON_LOCAL} --exec-prefix=${PYTHON_LOCAL}
 
@@ -26,14 +23,17 @@ make clean profile-removal
 make -j`nproc`
 make install
 
+file ${PYTHON_LOCAL}/bin/python3.8
 ln -s python3 ${PYTHON_LOCAL}/bin/python
 
-echo "print(\"Hello, World!\")" > ${working_dir}/hell_world.py
+export LD_LIBRARY_PATH=${PYTHON_LOCAL}/lib:${LD_LIBRARY_PATH}
+export PATH=${PYTHON_LOCAL}/bin:${PATH}
+python3 -VVV
 
-python3 ${working_dir}/hell_world.py
+
+echo "print(\"Hello, World!\")" > ${working_dir}/hell_world.py
 
 echo "Running hello world Program"
 python3 ${working_dir}/hell_world.py
 
-file ${PYTHON_LOCAL}/bin/python3.6
 
